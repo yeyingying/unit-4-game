e1e5574a63ecef7cc586d6922d344a4190e2bfd7
yeyingying <yyye88@gmail.com>
2019-05-15 14:56:46 -0400

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+
+
 var random_result;
 var lost = 0;
 var win = 0;
@@ -47,7 +49,6 @@ var resetAndStart = function () {
 resetAndStart();
 
 
-// Event Delegation
 $(document).on('click', ".crystal", function () {
 
 	var num = parseInt($(this).attr('data-random'));
@@ -85,13 +86,3 @@ $(document).on('click', ".crystal", function () {
 });
 
 
-// Speudo coding
-
-// a game with 4 crystal and Random Result
-// Every crystal needs to have a random number between 1 - 12
-// When clicking any CRYSTAL, it should adding with the previous result
-// Until it equals the Random Result
-// If it is greater than the Random Result, we decrement a lost counter
-// If it is equal, then we increment a win counter
-// A new random number should be generate every single time we win or lost
-// to those 4 crystals
